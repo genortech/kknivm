b5c94f02241828a932303146c5f4fbcb485d0924
lumprais <genortech@gmail.com>
2024-05-15 23:12:39 +1000

    update oil config

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,7 +1,20 @@
 return {
   {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+      columns = { 'icons' },
+      keymaps = {
+        ['<C-h>'] = false,
+        ['<M-h>'] = 'actions.select_split',
+      },
+      vim_options = {
+        show_hidden = true,
+      },
+      --Open parent directory in current window
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+      --Open parent directory in floating window
+      vim.keymap.set('n', '<SPACE>-', require('oil').toggle_float),
+    },
     -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
